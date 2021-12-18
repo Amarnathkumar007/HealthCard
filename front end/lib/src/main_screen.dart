5275bdbf53a09dd717830e0ba2e9666dab6d22f0
RIYA SREELAL <47321198+riyasreelal@users.noreply.github.com>
2021-12-18 15:32:31 +0530

    Update main_screen.dart

--- front end/lib/src/main_screen.dart
+++ front end/lib/src/main_screen.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'MEDICAL HEALTH APP',
       theme: ThemeData(
         primaryColor: primary,
       ),
